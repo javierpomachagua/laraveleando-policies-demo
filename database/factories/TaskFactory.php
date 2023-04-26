4b7dae95a5c43df54de5f391aef7aac19a9c3c32
javierpomachagua <javierpp0303@gmail.com>
2023-04-26 14:33:13 -0500

    add task factory

--- database/factories/TaskFactory.php
+++ database/factories/TaskFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\Task;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class TaskFactory extends Factory
@@ -12,7 +13,10 @@ class TaskFactory extends Factory
     public function definition(): array
     {
         return [
-
+            'title' => fake()->title,
+            'description' => fake()->text(200),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ];
     }
 }
